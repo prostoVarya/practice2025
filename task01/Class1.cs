992951e3e8d55d3c3a1285ea59cbbc6eb227038e
Varvara <bbmm999955@gmail.com>
2025-06-25 21:54:51 +0600

    Задание 1

---
+++ task01/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{ 
+    public static bool IsPalindrome(this string input)
+    {
+        if(string.IsNullOrEmpty(input)) return false;
+        input = new String(input.ToLower().Where(c => !char.IsPunctuation(c) && !char.IsWhiteSpace(c)).ToArray());
+        return input == new string(input.Reverse().ToArray());
+    }
+}
