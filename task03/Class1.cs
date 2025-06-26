caabdbb8c7c742cb1e61fabd3e409eed0405197d
Varvara <bbmm999955@gmail.com>
2025-06-26 20:44:56 +0600

    Задание 3

---
+++ task03/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CustomCollection<T> : IEnumerable<T>
+{
+    private readonly List<T> _items = new();
+
+    public void Add(T item) => _items.Add(item);
+    
+    public bool Remove(T item) => _items.Remove(item);
+    
+    public void Clear() => _items.Clear();
+    
+    public int Count => _items.Count;
+    
+    public IEnumerator<T> GetEnumerator() => _items.GetEnumerator();
+    
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    
+    // Метод для обратного обхода коллекции
+    public IEnumerable<T> GetReverseEnumerator()
+    {
+        for (int i = _items.Count - 1; i >= 0; i--)
+        {
+            yield return _items[i];
+        }
+    }
+    
+    // Статический метод для генерации числовой последовательности
+    public static IEnumerable<int> GenerateSequence(int start, int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            yield return start + i;
+        }
+    }
+    
+    // Метод для фильтрации и сортировки с помощью LINQ
+    public IEnumerable<T> FilterAndSort(Func<T, bool> predicate, Func<T, IComparable> keySelector)
+    {
+        return _items.Where(predicate).OrderBy(keySelector);
+    }
+}
