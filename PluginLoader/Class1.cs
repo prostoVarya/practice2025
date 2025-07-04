aa98e3cb6ec83b9c97f31d09fba5963b522bb597
Varvara <bbmm999955@gmail.com>
2025-07-04 21:32:36 +0600

    feat: task10

---
+++ PluginLoader/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+namespace PluginSystem
+{
+[AttributeUsage(AttributeTargets.Class)]
+public class PluginLoadAttribute : Attribute { }
+
+public interface IPluginCommand
+{
+    void Execute();
+}
+
+public class PluginLoader
+{
+    public void LoadAndExecutePlugins(string pluginsPath)
+    {
+        if (!Directory.Exists(pluginsPath))
+        {
+            Directory.CreateDirectory(pluginsPath);
+            Console.WriteLine($"Created plugins directory: {pluginsPath}");
+            return;
+        }
+
+        foreach (var dllPath in Directory.GetFiles(pluginsPath, "*.dll"))
+        {
+            try
+            {
+                Assembly assembly = Assembly.LoadFrom(dllPath);
+                
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.GetCustomAttribute<PluginLoadAttribute>() != null && 
+                        typeof(IPluginCommand).IsAssignableFrom(type))
+                    {
+                        try
+                        {
+                            IPluginCommand plugin = (IPluginCommand)Activator.CreateInstance(type);
+                            plugin.Execute();
+                            Console.WriteLine($"Executed plugin: {type.Name}");
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Failed to execute {type.Name}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to load {Path.GetFileName(dllPath)}: {ex.Message}");
+            }
+        }
+    }
+}
+}
---
+++ task10/Program.cs
@@ -0,0 +1,14 @@
+﻿using PluginSystem;
+class Program
+{
+    static void Main()
+    {
+        string pluginsPath = Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+        var loader = new PluginLoader();
+        
+        Console.WriteLine("Loading plugins...");
+        loader.LoadAndExecutePlugins(pluginsPath);
+        
+        Console.WriteLine("All plugins executed!");
+    }
+}
