1edd291555aa000d2af3790dc8f4be7865c97436
Varvara <bbmm999955@gmail.com>
2025-07-17 17:11:40 +0600

    feat: task17

---
+++ task17/Class1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using Command;
+namespace task17
+{
+    public class ServerThread : IDisposable
+    {
+        private Thread _workerThread;
+        private volatile bool _isRunning;
+        private volatile bool _softStopRequested;
+        private readonly BlockingCollection<ICommand> _commandQueue = new BlockingCollection<ICommand>();
+        private IExceptionHandler _exceptionHandler;
+
+        public void Start()
+        {
+            if (_workerThread != null && _workerThread.IsAlive)
+                return;
+
+            _isRunning = true;
+            _softStopRequested = false;
+            _workerThread = new Thread(ProcessCommands)
+            {
+                IsBackground = true,
+                Name = "ServerThread Worker"
+            };
+            _workerThread.Start();
+        }
+
+        public void AddCommand(ICommand command)
+        {
+            if (!_isRunning)
+                throw new InvalidOperationException("ServerThread не запущен");
+
+            _commandQueue.Add(command);
+        }
+
+        public void SetExceptionHandler(IExceptionHandler handler)
+        {
+            _exceptionHandler = handler;
+        }
+
+        private void ProcessCommands()
+        {
+            try
+            {
+                while (_isRunning)
+                {
+                    if (_commandQueue.TryTake(out ICommand command, Timeout.Infinite))
+                    {
+                        ExecuteCommand(command);
+                    }
+
+                    if (_softStopRequested && _commandQueue.Count == 0)
+                    {
+                        _isRunning = false;
+                    }
+                }
+            }
+            finally
+            {
+                _commandQueue.CompleteAdding();
+            }
+        }
+
+        private void ExecuteCommand(ICommand command)
+        {
+            try
+            {
+                command.Execute();
+            }
+            catch (Exception ex)
+            {
+                _exceptionHandler?.HandleException(command, ex);
+            }
+        }
+
+        public void RequestHardStop()
+        {
+            if (Thread.CurrentThread != _workerThread)
+                throw new InvalidOperationException("HardStop можно вызвать только из ServerThread");
+
+            _isRunning = false;
+        }
+
+        public void RequestSoftStop()
+        {
+            if (Thread.CurrentThread != _workerThread)
+                throw new InvalidOperationException("SoftStop можно вызвать только из ServerThread");
+
+            _softStopRequested = true;
+        }
+
+        public void Dispose()
+        {
+            _isRunning = false;
+            _commandQueue.Dispose();
+            _workerThread?.Join();
+        }
+    }
+
+    public interface IExceptionHandler
+    {
+        void HandleException(ICommand command, Exception exception);
+    }
+
+    public class HardStopCommand : ICommand
+    {
+        private readonly ServerThread _targetThread;
+
+        public HardStopCommand(ServerThread targetThread)
+        {
+            _targetThread = targetThread ?? throw new ArgumentNullException(nameof(targetThread));
+        }
+
+        public void Execute()
+        {
+            _targetThread.RequestHardStop();
+        }
+    }
+
+    public class SoftStopCommand : ICommand
+    {
+        private readonly ServerThread _targetThread;
+
+        public SoftStopCommand(ServerThread targetThread)
+        {
+            _targetThread = targetThread ?? throw new ArgumentNullException(nameof(targetThread));
+        }
+
+        public void Execute()
+        {
+            _targetThread.RequestSoftStop();
+        }
+    }
+}
---
+++ task17tests/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Xunit;
+using System;
+using System.Threading;
+using Command;
+using task17; 
+
+public class ServerThreadTests
+{
+    [Fact]
+    public void HardStop_ImmediatelyStopsThread()
+    {
+       
+        var server = new ServerThread();
+        var testCmd = new TestCommand();
+        
+        server.Start();
+        server.AddCommand(testCmd);
+        server.AddCommand(new HardStopCommand(server));
+        server.AddCommand(testCmd);
+        
+        Thread.Sleep(100);
+        
+        Assert.Equal(1, testCmd.ExecutionCount);
+    }
+
+    [Fact]
+    public void SoftStop_WaitsForQueueToEmpty()
+    {
+        var server = new ServerThread();
+        var testCmd = new TestCommand();
+        
+        server.Start();
+        server.AddCommand(testCmd);
+        server.AddCommand(new SoftStopCommand(server));
+        server.AddCommand(testCmd); 
+        
+        Thread.Sleep(150);
+        
+        Assert.Equal(2, testCmd.ExecutionCount);
+    }
+
+    [Fact]
+    public void Commands_ExecuteInOrder()
+    {
+        var server = new ServerThread();
+        var cmd1 = new TestCommand();
+        var cmd2 = new TestCommand();
+
+        server.Start();
+        server.AddCommand(cmd1);
+        server.AddCommand(cmd2);
+        
+        Thread.Sleep(100);
+        
+        Assert.Equal(1, cmd1.ExecutionCount);
+        Assert.Equal(1, cmd2.ExecutionCount);
+    }
+
+    private class TestCommand : ICommand
+    {
+        public int ExecutionCount { get; private set; }
+        
+        public void Execute() 
+        {
+            ExecutionCount++;
+            Thread.Sleep(20);
+        }
+    }
+}
